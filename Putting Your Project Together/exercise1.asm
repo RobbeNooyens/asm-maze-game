aeb7eae5007e79411dcc6661710980ca86deafd7
Robnoo02 <robbenooyens@hotmail.com>
2020-12-20 02:20:08 +0100

    Error in this version

--- Putting Your Project Together/exercise1.asm
+++ Putting Your Project Together/exercise1.asm
@@ -15,15 +15,56 @@ fileName: 	.asciiz "C:/Users/robbe/Documents/University/Bachelor 1/CSA/Mars/Proj
 fileContent:	.space 2048
 # Characters
 newLine: 	.asciiz "\n"
+# Coordinates
+playerRow:	.word 0
+playerColumn:	.word 0
 
 	.text
 main:
 	la	$a0, fileName
 	jal	load_maze
+	#li	$v0, 32
+	#li	$a0, 2000
+	#syscall
+	#lw	$a0, playerRow
+	#lw	$a1, playerColumn
+	#addiu	$a2, 
+	#jal	update_player_position
 	j exit
 
+# Updates player position
+# Parameters:
+#	a0: current player row
+#	a1: current player column
+#	a2: new player row
+#	a3: new player column
+update_player_position:
+	addi	$sp, $sp, -20
+	sw	$ra, 16($sp)
+	sw	$a3, 12($sp)
+	sw	$a2, 8($sp)
+	sw	$a1, 4($sp)
+	sw	$a0, 0($sp)
+	# Current player position address
+	jal 	coords_to_address
+	lw	$t1, black
+	sw	$v0, ($t0)
+	# New player position address
+	move	$a0, $a2
+	move	$a1, $a3
+	jal 	coords_to_address
+	lw	$t1, yellow
+	sw	$v0, ($t0)
+	# Restore values
+	lw	$ra, 16($sp)
+	lw	$a3, 12($sp)
+	lw	$a2, 8($sp)
+	lw	$a1, 4($sp)
+	lw	$a0, 0($sp)
+	addi	$sp, $sp, 20
+
 # Loads a maze based on the contents of a file
-# Input:
+# Parameters:
 # 	a0: asciiz; fileName
 # Stack:
 # 	$ra
@@ -87,6 +128,11 @@ load_bitmap:
 	# Store width and height in memory
 	sw	$v0, width
 	sw	$v1, height
+	# Save player column index
+	lw	$t0, playerColumn
+	div	$t0, $v0
+	mfhi	$t1
+	sw	$t1, playerColumn
 	# Load inital values
 	lw	$ra, 4($sp)
 	lw	$a0, 0($sp)
@@ -111,6 +157,11 @@ load_bitmap_loop:
 	beq	$t1, 10, load_bitmap_loop_newline
 	beq	$t1, 0, load_bitmap_loop_file_end
 	
+	# Save player coordinates
+	bne	$t1, 115, load_bitmap_loop_color_pixel
+	lw	$a2, playerRow
+	lw	$a1, playerColumn
+load_bitmap_loop_color_pixel:
 	# Save values
 	addi	$sp, $sp, -12
 	sw	$ra, 8($sp)
@@ -182,8 +233,27 @@ map_char_to_color_return:
 	lw	$t1, ($t0)
 	move	$v0, $t1
 	jr	$ra
-	
 
+# Map row and column to an address
+# Parameters:
+#	a0: column
+#	a1: row
+# Returns:
+#	v0: address
+coords_to_address:
+	# Load width and store y * 512 in register t0
+	lw 	$t0, width
+	mult 	$a1, $t0
+	mflo	$t0
+	# Add x
+	addu 	$t0, $t0, $a0
+	# Multiply by 4
+	sll	$t0, $t0, 2
+	# Add the result by the gp (bitmap start) address
+	addu	$v0, $gp, $t0
+	# Jump back
+	jr	$ra
+	
 # Prints a string
 # Parameters:
 #	a0: string; value to print
@@ -191,7 +261,6 @@ print_string:
 	li 	$v0,  4
 	syscall
 	jr	$ra
-
 # Prints an integer
 # Parameters:
 #	a0: word; integer to print
