a5abd6212e8c03de7c682dc8d1eaf027aa121892
Robnoo02 <robbenooyens@hotmail.com>
2020-12-20 04:09:17 +0100

    Backup

--- Putting Your Project Together/exercise1.asm
+++ Putting Your Project Together/exercise1.asm
@@ -23,48 +23,10 @@ playerColumn:	.word 0
 main:
 	la	$a0, fileName
 	jal	load_maze
-	#li	$v0, 32
-	#li	$a0, 2000
-	#syscall
-	#lw	$a0, playerRow
-	#lw	$a1, playerColumn
-	#addiu	$a2, 
-	#jal	update_player_position
 	j exit
 
-# Updates player position
-# Parameters:
-#	a0: current player row
-#	a1: current player column
-#	a2: new player row
-#	a3: new player column
-update_player_position:
-	addi	$sp, $sp, -20
-	sw	$ra, 16($sp)
-	sw	$a3, 12($sp)
-	sw	$a2, 8($sp)
-	sw	$a1, 4($sp)
-	sw	$a0, 0($sp)
-	# Current player position address
-	jal 	coords_to_address
-	lw	$t1, black
-	sw	$v0, ($t0)
-	# New player position address
-	move	$a0, $a2
-	move	$a1, $a3
-	jal 	coords_to_address
-	lw	$t1, yellow
-	sw	$v0, ($t0)
-	# Restore values
-	lw	$ra, 16($sp)
-	lw	$a3, 12($sp)
-	lw	$a2, 8($sp)
-	lw	$a1, 4($sp)
-	lw	$a0, 0($sp)
-	addi	$sp, $sp, 20
-
 # Loads a maze based on the contents of a file
-# Parameters:
+# Input:
 # 	a0: asciiz; fileName
 # Stack:
 # 	$ra
@@ -133,6 +95,10 @@ load_bitmap:
 	div	$t0, $v0
 	mfhi	$t1
 	sw	$t1, playerColumn
+	# Subtract 1 form playerRow to start counting from 0
+	lw	$t0, playerRow
+	addiu	$t0, $t0, -1
+	sw	$t0, playerRow
 	# Load inital values
 	lw	$ra, 4($sp)
 	lw	$a0, 0($sp)
@@ -157,16 +123,17 @@ load_bitmap_loop:
 	beq	$t1, 10, load_bitmap_loop_newline
 	beq	$t1, 0, load_bitmap_loop_file_end
 	
-	# Save player coordinates
-	bne	$t1, 115, load_bitmap_loop_color_pixel
-	lw	$a2, playerRow
-	lw	$a1, playerColumn
-load_bitmap_loop_color_pixel:
 	# Save values
 	addi	$sp, $sp, -12
 	sw	$ra, 8($sp)
 	sw	$a1, 4($sp)
 	sw	$a0, 0($sp)
+	
+	# Save player coordinates
+	bne	$t1, 115, load_bitmap_loop_color_pixel
+	sw	$a2, playerRow
+	sw	$a1, playerColumn
+load_bitmap_loop_color_pixel:
 	# Set pixel to right color
 	move	$a0, $t1
 	jal 	map_char_to_color
@@ -234,26 +201,7 @@ map_char_to_color_return:
 	move	$v0, $t1
 	jr	$ra
 
-# Map row and column to an address
-# Parameters:
-#	a0: column
-#	a1: row
-# Returns:
-#	v0: address
-coords_to_address:
-	# Load width and store y * 512 in register t0
-	lw 	$t0, width
-	mult 	$a1, $t0
-	mflo	$t0
-	# Add x
-	addu 	$t0, $t0, $a0
-	# Multiply by 4
-	sll	$t0, $t0, 2
-	# Add the result by the gp (bitmap start) address
-	addu	$v0, $gp, $t0
-	# Jump back
-	jr	$ra
-	
+
 # Prints a string
 # Parameters:
 #	a0: string; value to print
@@ -261,6 +209,7 @@ print_string:
 	li 	$v0,  4
 	syscall
 	jr	$ra
+
 # Prints an integer
 # Parameters:
 #	a0: word; integer to print
@@ -273,4 +222,3 @@ print_int:
 exit:
 	li	$v0, 10
 	syscall
-
---
+++ Putting Your Project Together/exercise2.asm
@@ -0,0 +1,113 @@
+	.data
+width:		.word 8
+height:		.word 8
+blue:		.word 0x000000ff
+black:		.word 0x00000000
+yellow:		.word 0x00ffff00
+
+	.text
+
+main:
+	li	$a0, 1
+	li	$a1, 6
+	li	$a2, 1
+	li	$a3, 5
+	jal update_player_position
+	move	$a0, $v0
+	move	$t0, $v1
+	jal print_int
+	move	$a0, $t0
+	jal print_int
+	j exit
+
+# Updates player position
+# Parameters:
+#	a0: current player row
+#	a1: current player column
+#	a2: new player row
+#	a3: new player column
+# Returns:
+#	v0: actual new row
+#	v1: actual new column
+update_player_position:
+	addi	$sp, $sp, -28
+	sw	$ra, 24($sp)
+	sw	$s1, 20($sp)
+	sw	$s0, 16($sp)
+	sw	$a3, 12($sp)
+	sw	$a2, 8($sp)
+	sw	$a1, 4($sp)
+	sw	$a0, 0($sp)
+	move	$t0, $a0
+	move	$t1, $a1
+	# Current player position address
+	jal 	coords_to_address
+	move	$s0, $v0
+	# New player position address
+	move	$a0, $a2
+	move	$a1, $a3
+	jal 	coords_to_address
+	move	$s1, $v0
+	# Check validity
+	move	$v0, $t0
+	move	$v1, $t1
+	blt	$a0, 0, update_player_restore
+	blt	$a1, 0, update_player_restore
+	lw	$t0, width
+	lw	$t1, height
+	bge	$a0, $t1, update_player_restore
+	bge	$a1, $t0, update_player_restore
+	lw	$t3, ($s1)
+	lw	$t4, blue
+	beq	$t3, $t4, update_player_restore # new spot is wall
+	move	$v0, $a0
+	move	$v1, $a1
+	# Color pixels if position is valid
+	lw	$t1, black
+	sw	$t1, ($s0)
+	lw	$t1, yellow
+	sw	$t1, ($s1)
+update_player_restore:
+	# Restore values
+	lw	$ra, 24($sp)
+	lw	$s1, 20($sp)
+	lw	$s0, 16($sp)
+	lw	$a3, 12($sp)
+	lw	$a2, 8($sp)
+	lw	$a1, 4($sp)
+	lw	$a0, 0($sp)
+	addi	$sp, $sp, 28
+	jr	$ra
+
+# Map row and column to an address
+# Parameters:
+#	a0: row
+#	a1: column
+# Returns:
+#	v0: address
+coords_to_address:
+	# Load width and store y * 512 in register t0
+	lw 	$t0, width
+	mult 	$a0, $t0
+	mflo	$t0
+	# Add x
+	addu 	$t0, $t0, $a1
+	# Multiply by 4
+	sll	$t0, $t0, 2
+	# Add the result by the gp (bitmap start) address
+	addu	$v0, $gp, $t0
+	# Jump back
+	jr	$ra
+
+# Prints a string
+# Parameters:
+#	a0: string; value to print
+print_int:
+	li 	$v0,  1
+	syscall
+	jr	$ra
+
+# Terminates program
+exit:
+	li	$v0, 10
+	syscall
